cca902a8b7353ef316ba7e70a2ef829844c203bc
Emily Salinas <emilyrh1058@gmail.com>
2021-03-18 22:45:06 -0500

    add col and id's to timeblocks

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var containerEl = document.querySelector(".container")
-console.log(containerEl)
-
 var currentDayEl = document.getElementById("currentDay")
-console.log(currentDayEl)
-
 function loadCurrent() {
     var currentDay = moment().format("ddd, MMMM Do YYYY");
     currentDayEl.textContent = currentDay
