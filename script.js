2a751d8754d9c10143e79b57205d57a1418db7d3
Emily Salinas <emilyrh1058@gmail.com>
2021-04-04 22:46:23 -0500

    tried to fix text areas. added min and sec to current time

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-var containerEl = document.querySelector(".container")
-var currentDayEl = document.getElementById("currentDay")
 function loadCurrent() {
-    var currentDay = moment().format("ddd, MMMM Do YYYY");
-    currentDayEl.textContent = currentDay
+    var displayCurrent = document.getElementById("currentDay")
+    var todayDateTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    displayCurrent.innerHTML = todayDateTime
 }
 loadCurrent();
 
-var beginningTime = moment().format("HH")
-console.log(beginningTime)
+$("currentDay").html(todayDateTime);
 
-$(document).ready(function () {
-    
+$(document).ready(function() {
+
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
     })
-}); 
 
 function timeTracker() {
-        
+    var beginningTime = moment().hour();
 
     $(".time-block").each(function () {
         var blockTime = parseInt($(this).attr("id").split("hour")[1]);
@@ -39,18 +36,20 @@ function timeTracker() {
             $(this).removeClass("present");
             $(this).removeClass("past");
             $(this).addClass("future");
-
         }
     })
 }
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
 $("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
 
 timeTracker();
+})
